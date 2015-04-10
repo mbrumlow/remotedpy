88d8f9d6f6bc11f3610e35fca13af07e63084882
Michael Brumlow <mbrumlow@gmail.com>
2015-04-09 19:36:43 -0500

    Break out C code and clean it up some.

---
+++ x11/x11.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/Xdamage.h>
+#include <X11/extensions/Xcomposite.h>
+#include <X11/keysym.h>
+#include <X11/extensions/XTest.h>
+
+Pixmap windowPix;
+
+void DestroyImage(XImage *img) {
+	XDestroyImage(img);
+}
+
+void GetScreenSize(Display *dpy, int *width, int *height)  {
+	int screen = DefaultScreen(dpy);
+    *width = DisplayWidth(dpy, screen);
+    *height = DisplayHeight(dpy, screen);
+}
+
+unsigned long GetPixel(XImage *img, int x, int y) {
+	return XGetPixel(img, x, y);
+}
+
+static int xerror_handler(Display *dpy, XErrorEvent *e) {
+    return 0;
+}
+
+static void register_damage(Display *dpy, Window win) {
+    XWindowAttributes attrib;
+    if (XGetWindowAttributes(dpy, win, &attrib) && !attrib.override_redirect) {
+        XDamageCreate(dpy, win, XDamageReportRawRectangles);
+    }
+}
+
+void RegisterDamanges(Display *dpy) {
+
+	Window root = DefaultRootWindow(dpy);
+	XSelectInput(dpy, root, SubstructureNotifyMask);
+
+    Window rootp, parent;
+    Window *children;
+    unsigned int i, nchildren;
+    XQueryTree(dpy, root, &rootp, &parent, &children, &nchildren);
+
+    XSetErrorHandler(xerror_handler);
+
+ 	XCompositeRedirectSubwindows( dpy, RootWindow( dpy, 0 ), CompositeRedirectAutomatic );
+
+	register_damage(dpy, root);
+	for (i = 0; i < nchildren; i++) {
+        register_damage(dpy, children[i]);
+	}
+
+    XFree(children);
+}
+
+void SendKey(Display *dpy, unsigned int key, int down) {
+
+	Bool d = False;
+	unsigned int kc = XKeysymToKeycode(dpy, key);
+
+	if(down == 1) {
+		d = True;
+	}
+	XTestFakeKeyEvent(dpy, kc, d, 0);
+	XFlush(dpy);
+}
+
+void SendMouseClick(Display *dpy, int button, unsigned x, unsigned y, int down) {
+	Bool d = False;
+	if( down == 1) {
+		d = True;
+	}
+	XTestFakeMotionEvent(dpy, 0, x, y, 0);
+	XTestFakeButtonEvent(dpy, button, d, 0);
+	XFlush(dpy);
+}
+
+void SendMouseMove(Display *dpy, unsigned x, unsigned y) {
+	XTestFakeMotionEvent(dpy, 0, x, y, 0);
+	XFlush(dpy);
+}
+
+XImage *GetDamage(Display *dpy, int damageEvent, int *x, int *y, int *h, int *w)  {
+
+    XEvent ev;
+
+    int screen = DefaultScreen(dpy);
+
+	while(1) {
+
+        XNextEvent(dpy, &ev);
+
+		if(ev.type == MapNotify){
+            do { // Gobble up all these event types.
+                register_damage(dpy, ev.xcreatewindow.window);
+            } while ( XCheckTypedEvent(dpy, MapNotify, &ev) ) ;
+			 continue;
+		}
+
+		if( ev.type == damageEvent + XDamageNotify ) {
+
+			int rx = 0;
+			int ry = 0;
+			int fr = 0;
+			int damage = 0;
+
+			do { // Gobble up the rest of the damage events.
+
+                XDamageNotifyEvent  *dev = (XDamageNotifyEvent *) &ev;
+				// TODO: split large non-overlapping regions up instead of
+				// of making one large region.
+
+
+				if(fr == 0) {
+					*x = dev->area.x;
+					*w = dev->area.width;
+					rx = dev->area.x + dev->area.width;
+				} else if(dev->area.x < *x) {
+        			*x = dev->area.x;
+				}
+
+				if(fr == 0) {
+					*y = dev->area.y;
+					*h = dev->area.height;
+					ry = dev->area.y + dev->area.height;
+				} else if(dev->area.y < *y) {
+        			*y = dev->area.y;
+				}
+
+				if( dev->area.y + dev->area.height > ry ) {
+					ry = dev->area.y + dev->area.height;
+					*h = ry - *y;
+				}
+
+				if( dev->area.x + dev->area.width > rx ) {
+					rx = dev->area.x  + dev->area.width;
+					*w = rx - *x;
+				}
+
+                // TODO: Figure out what -1 really means.
+				if(*x < 0) *x = 0;
+				if(*y < 0) *y = 0;
+
+				fr = 1;
+        		damage = 1;
+				XDamageSubtract( dpy, dev->damage, None, None );
+
+            } while (XCheckTypedEvent(dpy, damageEvent + XDamageNotify, &ev));
+
+			if(damage) {
+
+				*h = ry - *y;
+				*w = rx - *x;
+
+				XImage *i = XGetImage(dpy,
+					DefaultRootWindow(dpy),
+                	*x, *y, *w, *h,
+                	AllPlanes, ZPixmap);
+
+				return i;
+			}
+    	}
+	}
+
+	return NULL;
+}
+
